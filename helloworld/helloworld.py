a2fc8728b2546731ab9f63c02ec3006b23e34478
Ryan Hess <ryan@vacationcandy.com>
2013-02-24 13:18:53 -0700

    Added app.yaml etc to healthyhabits, started user integration

--- helloworld/helloworld.py
+++ helloworld/helloworld.py
@@ -1,11 +1,17 @@
 import webapp2
 import os
 from google.appengine.ext.webapp import template
+from google.appengine.api import users
 
 class MainPage(webapp2.RequestHandler):
   def get(self):
-    self.response.headers['Content-Type'] = 'text/html'
-    self.response.out.write(template.render("templates/home.html", {'lname': "Hess", 'uname': 'noobile'}))
+    user = users.get_current_user();
+    
+    if user:
+      self.response.headers['Content-Type'] = 'text/html'
+      self.response.out.write(template.render("templates/home.html", {'lname': "Hess", 'uname': user.nickname()}))
+    else:
+      self.redirect(users.create_login_url(self.request.uri));
     
 app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
     
