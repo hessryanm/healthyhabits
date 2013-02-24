8119b9d3b5d40619184136a05753cc44b33eb579
Ryan Hess <ryan@vacationcandy.com>
2013-02-24 15:55:02 -0700

    Fixed models

--- healthyhabits/home.py
+++ healthyhabits/home.py
@@ -1,15 +1,32 @@
 import webapp2
 import os
+import logging
 from google.appengine.ext.webapp import template
 from google.appengine.api import users
 from models import *
+import datetime
 
 class MainPage(webapp2.RequestHandler):
   def get(self):
     google_user = users.get_current_user()
-    #user = User.gql("WHERE user_id IS :1 LIMIT 1", google_user.user_id())
+    user = UserRecord.gql("WHERE user_id = :1", google_user.user_id())
+    user = user.get()
+    if user is None:
+      user = UserRecord()
+      user.user_id = google_user.user_id()
+      user.name = google_user.nickname()
+      user.put()
+    q = UserRecord.all()
+    q.order("name")
+    all_users = []
+    for p in q.run():
+      person = {}
+      person['name'] = p.name
+      print(datetime.datetime.now().date())
+      week = WeekRecord.gql("WHERE start < :1 AND end > :1", datetime.datetime.now().date())
+      week = week.get()
     self.response.headers['Content-Type'] = 'text/html'
-    self.response.out.write(template.render("templates/home.html", {'id': google_user.user_id(), 'uname': google_user.nickname()}))
+    self.response.out.write(template.render("templates/home.html", {'uname': user.name}))
     
 class SubmitPage(webapp2.RequestHandler):
   def get(self):
--- healthyhabits/models.py
+++ healthyhabits/models.py
@@ -1,34 +1,35 @@
-# import cgi
-# import datetime
-# import urllib
-# import webapp2
+import cgi
+import datetime
+import urllib
+import webapp2
 
-# from google.appengine.ext import db
+from google.appengine.ext import db
 
-# class UserRecord(db.Model):
-#   user_id = db.IntegerProperty()
-#   name = db.StringProperty()
+class UserRecord(db.Model):
+  user_id = db.StringProperty()
+  name = db.StringProperty()
   
-# class DayRecord(db.Model):
-#   sleepAmount = db.BooleanProperty(default=False)
-#   sleepTimes = db.BooleanProperty(default=False)
-#   nap = db.BooleanProperty(default=False)
-#   exercise = db.BooleanProperty(default=False)
-#   veggies = db.BooleanProperty(default=False)
-#   healthyMeals = db.BooleanProperty(default=False)
-#   mealTimes = db.BooleanProperty(default=False)
-#   junkFood = db.BooleanProperty(default=False)
-#   relax = db.BooleanProperty(default=False)
-#   water = db.BooleanProperty(default=False)
-#   total = db.IntegerProperty()
+class DayRecord(db.Model):
+  sleepAmount = db.BooleanProperty(default=False)
+  sleepTimes = db.BooleanProperty(default=False)
+  nap = db.BooleanProperty(default=False)
+  exercise = db.BooleanProperty(default=False)
+  veggies = db.BooleanProperty(default=False)
+  healthyMeals = db.BooleanProperty(default=False)
+  mealTimes = db.BooleanProperty(default=False)
+  junkFood = db.BooleanProperty(default=False)
+  relax = db.BooleanProperty(default=False)
+  water = db.BooleanProperty(default=False)
+  total = db.IntegerProperty()
 
-# class WeekRecord(db.Model):
-#   start = db.DateProperty()
-#   end = db.DateProperty()
-#   user = db.ReferenceProperty(UserRecord)
-#   mon = db.ReferenceProperty(DayRecord)
-#   tues = db.ReferenceProperty(DayRecord)
-#   wed = db.ReferenceProperty(DayRecord)
-#   thurs = db.ReferenceProperty(DayRecord)
-#   fri = db.ReferenceProperty(DayRecord)
-#   sat = db.ReferenceProperty(DayRecord)
+class WeekRecord(db.Model):
+  start = db.DateProperty()
+  end = db.DateProperty()
+  user = db.ReferenceProperty(UserRecord)
+  mon = db.ReferenceProperty(DayRecord, collection_name="mon")
+  tues = db.ReferenceProperty(DayRecord, collection_name="tues")
+  wed = db.ReferenceProperty(DayRecord, collection_name="wed")
+  thurs = db.ReferenceProperty(DayRecord, collection_name="thurs")
+  fri = db.ReferenceProperty(DayRecord, collection_name="fri")
+  sat = db.ReferenceProperty(DayRecord, collection_name="sat")
+
