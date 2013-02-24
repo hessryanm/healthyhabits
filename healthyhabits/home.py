67abb16e4bc70363d34010f9bfce88ed1a610e07
Ryan Hess <ryan@vacationcandy.com>
2013-02-24 14:38:31 -0700

    Added handler for submit page

--- healthyhabits/home.py
+++ healthyhabits/home.py
@@ -2,11 +2,21 @@ import webapp2
 import os
 from google.appengine.ext.webapp import template
 from google.appengine.api import users
+from models import *
 
 class MainPage(webapp2.RequestHandler):
   def get(self):
+    google_user = users.get_current_user()
+    user = User.gql("WHERE user_id IS :1 LIMIT 1", google_user.user_id())
     self.response.headers['Content-Type'] = 'text/html'
-    self.response.out.write(template.render("templates/home.html", {'lname': "Hess", 'uname': 'noobile'}))
+    self.response.out.write(template.render("templates/home.html", {'id': user.user_id(), 'uname': user.nickname()}))
     
-app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
+class SubmitPage(webapp2.RequestHandler):
+  def get(self):
+    self.response.out.write(template.render("templates/submit.html", {}))
+    
+app = webapp2.WSGIApplication([
+  ('/', MainPage), 
+  ('/submit', SubmitPage)], 
+debug=True)
     
