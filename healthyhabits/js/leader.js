4a85ea1b0e1ab35fb1b98c102d0e1f4363ea9025
Ryan Hess <ryan@Hess-Macbook-Moki.local>
2013-02-25 17:52:48 -0700

    leader fixed

--- healthyhabits/js/leader.js
+++ healthyhabits/js/leader.js
@@ -4,11 +4,14 @@ $(function(){
 	$(".total").each(function(){
 		var val = $(this).html();
 		val = parseInt(val);
-		if (val >= highest){
-			highest = val;
+		if (val == highest) {
 			var user = $(this).closest("tr").children(":first").text();
 			if ($(".winning").text() != "") $(".winning").append("<br/>");
 			$(".winning").append(user);
+		} else if (val > highest){
+			highest = val;
+			var user = $(this).closest("tr").children(":first").text();
+			$(".winning").text(user);
 		} 
 	});
 });
